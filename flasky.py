ac24e4b6f85b7a6254cd641627a1df29e4b8cd84
liuyang19900520 <liuyang19900520@hotmail.com>
2019-01-28 14:50:46 +0900

    Chapter 7 finished * code version is later than book. use flask-cli instead of flask-script

--- flasky.py
+++ flasky.py
@@ -1,23 +1,15 @@
 import os
-
-from flask_migrate import Migrate, MigrateCommand
-from flask_script import Manager, Shell
-
+from flask_migrate import Migrate
 from app import create_app, db
 from app.models import User, Role
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
-manager = Manager(app)
 
 
 @app.shell_context_processor
 def make_shell_context():
     return dict(db=db, User=User, Role=Role)
-
-
-manager.add_command("shell", Shell(make_context=make_shell_context))
-manager.add_command('db', MigrateCommand)
 
 
 @app.cli.command()
