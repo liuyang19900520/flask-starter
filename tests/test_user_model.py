b7066debe010de72754b3c40d3fc4c8859dfdcac
liuyang19900520 <liuyang19900520@hotmail.com>
2019-02-03 17:08:39 +0900

    Chapter 10 * user user info message add

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -1,4 +1,5 @@
 import unittest
+
 from app import create_app, db
 from app.models import User, Role, Permission, AnonymousUser
 
@@ -43,3 +44,16 @@ class UserModelTestCase(unittest.TestCase):
     def test_anonymous_user(self):
         u = AnonymousUser()
         self.assertFalse(u.can(Permission.FOLLOW))
+
+    def test_gravatar(self):
+        u = User(email='john@example.com', password='cat')
+        with self.app.test_request_context('/'):
+            gravatar = u.gravatar()
+            gravatar_256 = u.gravatar(size=256)
+            gravatar_pg = u.gravatar(rating='pg')
+            gravatar_retro = u.gravatar(default='retro')
+        self.assertTrue('https://secure.gravatar.com/avatar/' +
+                        'd4c74594d841139328695756648b6bd6' in gravatar)
+        self.assertTrue('s=256' in gravatar_256)
+        self.assertTrue('r=pg' in gravatar_pg)
+        self.assertTrue('d=retro' in gravatar_retro)
